31b8397c2c7206a8c7c41122c5daa6b93b4acbf2
weslygio <16520363@mahasiswa.itb.ac.id>
2021-11-24 18:33:53 +0700

    j-5.5 (liveshare again)

---
+++ tc1.py
@@ -0,0 +1,11 @@
+x = 7
+
+if x == 3:
+    return 1
+elif x == 5:
+    return 1
+elif x == 7:
+    return 1
+else:
+    return 0
+
---
+++ tes6.py
@@ -0,0 +1,12 @@
+class tes:
+    """ini class coba"""
+    
+    def nama(nama):
+        return nama
+        
+    # yuk bisa yuk
+
+
+Teguh = tes()
+
+print(Teguh.nama())
